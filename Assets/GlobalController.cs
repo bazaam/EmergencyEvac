73b61ddd5c018691be2d86cba59a3f538008304e
Reinboom <reina.sweet@riotgames.com>
2017-01-21 16:57:22 -0800

    Created global listener and click handler

---
+++ Assets/GlobalController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalController
+{
+	public static GlobalController instance = new GlobalController();
+
+	private List<MeepleController2D> mMeeples = new List<MeepleController2D>();
+
+    GlobalController()
+    {
+        //
+    }
+
+    public void Update()
+    {
+        if (UnityEngine.Input.GetMouseButton(1))
+        {
+            foreach (MeepleController2D meeple in mMeeples)
+            {
+                meeple.AlertMeeple(UnityEngine.Input.mousePosition);
+            }
+        }
+    }
+
+    public void RegisterMeeple(MeepleController2D meeple)
+    {
+        mMeeples.Add(meeple);
+    }
+}
--- Assets/MeepleController2D.cs
+++ Assets/MeepleController2D.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class MeepleController2D : MonoBehaviour {
 
+    private float offsetForDebugging = 50.0f;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        Invoke("RegisterThis", 1.0f);
 	}
+
+    void RegisterThis()
+    {
+        GlobalController.instance.RegisterMeeple(this);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        GlobalController.instance.Update();
 	}
+
+    public void AlertMeeple(Vector3 clickPosition)
+    {
+        offsetForDebugging *= -1;
+        transform.Translate(new Vector3(offsetForDebugging, offsetForDebugging, 0.0f));
+    }
 }
