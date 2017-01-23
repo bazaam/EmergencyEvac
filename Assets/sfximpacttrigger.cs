d10d47fca037617c4ffd080dadb36b9987ae69c7
Smoose3 <nickdeboisblanc@gmail.com>
2017-01-22 21:14:54 -0800

    Audio Mixer & MeepleImpact effect script

--- Assets/sfximpacttrigger.cs
+++ Assets/sfximpacttrigger.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sfximpacttrigger : MonoBehaviour {
-
+public class sfximpacttrigger : MonoBehaviour
+{
+    AudioSource AudioPlayer;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        AudioPlayer = gameObject.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		 
 	}
+    void OnTriggerEnter(Collider other)
+    {
+        // When a meeple hits a wall play MeepleWallImpact sound effect once
+        if(other.tag == "Meeple")
+        {
+            AudioPlayer.Play();
+        }
+    }
 }
